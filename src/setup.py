aec9544532d946e6b58131333abc9170697f5d00
bencarle <bencarle@gmail.com>
2015-07-21 14:04:03 -0700

    Updated setup.

--- src/setup.py
+++ src/setup.py
@@ -8,33 +8,13 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-from setuptools.command.test import test as TestCommand
-import sys
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = []
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(self.pytest_args)
-        sys.exit(errno)
-
 from setuptools import setup
 setup(
-    name='cosc-learning-labs',
-    description='Cisco Open SDN Controller (COSC) Learning Lab',
+    name='odl-learning-labs',
+    description='OpenDaylight Application Development Learning Lab Scripts using Python and RESTCONF',
     version='1.0',
-    packages=['basics', 'learning_lab', 'settings', ],
-    url='http://github.com/CiscoDevNet/cosc-learning-labs',
+    packages=['basics', 'labs', 'settings', ],
+    url='http://github.com/bencarle/odl-learning-labs',
     license='Apache License, Version 2.0',
     long_description=read('../README.md'),
     install_requires=[
@@ -46,6 +26,4 @@ setup(
         "future",
         "pexpect",
     ],
-    tests_require=['pytest','testfixtures'],
-    cmdclass = {'test': PyTest},
 )
